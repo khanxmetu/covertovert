f72e887b1d74d9bc4755a0741079e0b87fa2d19b
khanxmetu <khanxbahria@gmail.com>
2024-10-24 17:49:58 +0300

    feat(sender, receiver): implement sender and receiver according to the specifications

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,33 @@
-import scapy
+from typing import Optional
 
-# Implement your ICMP receiver here
+from scapy.all import IP, Packet, sniff
+
+
+def format_callback(packet: Packet) -> Optional[str]:
+    """Formats the output to be displayed for the packet.
+
+    This callback function is intended to be used as an argument to `scapy.sendrecv.sniff`.
+    Note that the ttl-based filtering (ttl=1) is done here instead of `filter` argument
+    to `scapy.sendrecv.sniff` due to the limatations of Berkeley Packet Filter syntax.
+
+
+    Returns:
+        The formatted string to show the packet.
+    """
+    if packet[IP].ttl != 1:
+        return
+
+    return packet.show()
+
+
+def run_receiver() -> None:
+    """Starts the sniffer to filter and print ICMP packets from sender host with ttl=1"""
+    sniff(
+        prn=format_callback,
+        filter="icmp and src host sender and dst host receiver",
+        store=0,
+    )
+
+
+if __name__ == "__main__":
+    run_receiver()
--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,11 @@
-import scapy
+from scapy.all import ICMP, IP, send
 
-# Implement your ICMP sender here
+
+def send_pkt() -> None:
+    """Sends an ICMP packet with empty payload to receiver host with ttl=1"""
+    pkt = IP(dst="receiver", ttl=1) / ICMP()
+    send(pkt)
+
+
+if __name__ == "__main__":
+    send_pkt()
