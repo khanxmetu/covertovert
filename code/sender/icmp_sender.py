c96ee97c02b2668b3e08635f588b724e0aafd994
khanxmetu <khanxbahria@gmail.com>
2024-10-28 15:13:55 +0300

    refactor(icmp_sender): refactor variable names for clarity

--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,11 +1,11 @@
 from scapy.all import ICMP, IP, send
 
 
-def send_pkt() -> None:
+def send_target_packet() -> None:
     """Sends an ICMP packet with empty payload to receiver host with ttl=1"""
-    pkt = IP(dst="receiver", ttl=1) / ICMP()
-    send(pkt)
+    target_packet = IP(dst="receiver", ttl=1) / ICMP()
+    send(target_packet)
 
 
 if __name__ == "__main__":
-    send_pkt()
+    send_target_packet()
