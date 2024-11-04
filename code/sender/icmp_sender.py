a18e79821a140974ca1bf0d2823ec5a6f629ec44
khanxmetu <khanxbahria@gmail.com>
2024-11-04 12:03:05 +0300

    Phase1 (#2)
    * chore: add devcontainer support
    
    * feat(sender, receiver): implement sender and receiver according to the specifications
    
    * fix(receiver)!: Exit after receiving single packet from sender
    refactor(receiver): Wrap into a class
    
    * docs(receiver): fix typo in docstring
    
    * build(docs): add workflow to build documentation and deploy to github pages
    
    * refactor(icmp_receiver): decouple packet filtering from prn callback
    fix(icmp_receiver): add ICMP echo-request filter
    
    * refactor(icmp_sender): refactor variable names for clarity
    
    * docs(receiver): fix docstring formatting
    
    * build(docs): install scapy in the workflow for doc generation
    
    * refactor(receiver): use BPF filtering instead for better performance
    since the filter is applied inside the kernel
    
    * docs: add assignment group and author info

--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,11 @@
-import scapy
+from scapy.all import ICMP, IP, send
 
-# Implement your ICMP sender here
+
+def send_target_packet() -> None:
+    """Sends an ICMP packet with empty payload to receiver host with ttl=1"""
+    target_packet = IP(dst="receiver", ttl=1) / ICMP()
+    send(target_packet)
+
+
+if __name__ == "__main__":
+    send_target_packet()
